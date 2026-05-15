de3ad03930d8f946faca0c514464ad76d8b1714d
Ramezali99 <nexoralabs99@gmail.com>
2026-05-15 09:36:35 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-red-500 mb-4">
+          Nexora Labs 🔥
+        </h1>
+
+        <p className="text-gray-400 text-xl">
+          Website Running Successfully
+        </p>
+
+        <button className="mt-6 bg-red-600 hover:bg-red-700 px-6 py-3 rounded-xl font-bold">
+          Start
+        </button>
+      </div>
+    </div>
+  );
+}
